6301a620c56f0c03cc6e219c1589f418787e0fe3
kevin131372 <kevincevalloszambrano@gmail.com>
2022-07-21 14:02:49 -0500

    cambios

--- core/models.py
+++ core/models.py
@@ -1,6 +1,16 @@
+from turtle import update
+from venv import create
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField()
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now_add=True)
+
 class User(models.Model):
     nombres = models.CharField(max_length=150)
     apellidos = models.CharField(max_length=150)
