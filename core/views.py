8f0f6842979f029580c167f08a50405952e65edc
kevin131372 <kevincevalloszambrano@gmail.com>
2022-07-28 18:59:33 -0500

    cambios de proyecto

--- core/views.py
+++ core/views.py
@@ -1,19 +1,23 @@
-from email import message
+from django.contrib import messages
 from multiprocessing import context
 from tkinter.tix import Form
 from urllib import response
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import *
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
-def register (request):   
-    new_nombres = request.POST.get('name','')
-    new_apellidos = request.POST.get("lastname",'') 
-    new_correo = request.POST.get("correo",'') 
-    new_password = request.POST.get("password",'') 
-    user = User(nombres=new_nombres,apellidos=new_apellidos,correo=new_correo,password=new_password)
-    user.save()
+def register (request):
+    if request.method =='POST':
+        new_nombres = request.POST.get('name','')
+        new_apellidos = request.POST.get("lastname",'')
+        new_correo = request.POST.get("correo",'')
+        new_password = request.POST.get("password",'')
+        user = User(nombres=new_nombres,apellidos=new_apellidos,correo=new_correo,password=new_password)
+        user.save()
+        return redirect('recetas')
+    else:
+        User()
     return render(request, 'core/register.html')
 
 def index (request):
